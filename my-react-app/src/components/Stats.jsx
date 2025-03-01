85547175172fcd7cd0d647e29382acb45f7e55f1
sgjohnson455 <144621442+sgjohnson455@users.noreply.github.com>
2025-03-01 23:33:40 +0000

    added images to the stats and centered the image

--- my-react-app/src/components/Stats.jsx
+++ my-react-app/src/components/Stats.jsx
@@ -4,11 +4,11 @@ export default function Stats({}) {
     return (
         <div className={styles.layout}>
             <div className={styles.layout} style={{gap: '0.5rem', transform: 'translateY(0rem)'}}>
-                <h3>💰</h3>
+                <img src="/src/assets/Images/heart-icon.png" alt="Coin Icon" style={{width: '40px', height: '40px'}} />
                 <p id={`GOLD`}>100</p>
             </div>
             <div className={styles.layout} style={{gap: '0.5rem', transform: 'translateY(0rem)'}}>
-                <h3>👤</h3>
+                <img src="/src/assets/Images/parchment-icon.png" alt="Coin Icon" style={{width: '40px', height: '40px'}} />
                 <p id={`PEOPLE`}>100</p>
             </div>
             <div className={styles.layout} style={{gap: '0.0rem', transform: 'translateY(0rem)'}}>
