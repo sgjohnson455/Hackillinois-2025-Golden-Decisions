2af32682800ca19447c87da42d1c06b80b866ccb
Rachel Li <rli60@illinois.edu>
2025-02-28 22:20:13 -0600

    Push again.

---
+++ my-react-app/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import styles from './Welcome.module.css';
+
+export default function Welcome() {
+    return (
+        <div>
+            <h1 className={styles.text}>WELCOME</h1>
+        </div>
+        
+    )
+}
