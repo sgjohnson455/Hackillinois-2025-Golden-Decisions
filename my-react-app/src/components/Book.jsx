b1d2d289164f1f3930040676619a290a7c1056b1
sjohn241 <sjohn241@illinois.edu>
2025-03-02 05:45:05 -0600

    changed book to be more descriptive button

--- my-react-app/src/components/Book.jsx
+++ my-react-app/src/components/Book.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import styles from './Book.module.css';
 
 export default function Book() {
-    
-    function openBook({url='https://drive.google.com/file/d/13aUvdF5YABblhYzETMN3uIkw-wMMv22L/view?usp=sharing', windowName = '_blank'}) {
+
+    function openBook({ url = 'https://drive.google.com/file/d/13aUvdF5YABblhYzETMN3uIkw-wMMv22L/view?usp=sharing', windowName = '_blank' }) {
         // const handleClick = () => {
-            window.open(url, windowName);
+        window.open(url, windowName);
         // };
     }
 
     return (
         <>
-            <button className={styles.button} onClick={openBook}>BOOK</button>
+            <button className={styles.button} onClick={openBook}>DIONYSUS'S BOOK</button>
         </>
     )
 }
