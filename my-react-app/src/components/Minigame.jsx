39447a2672251dde2a5d63c4a25d4d2f1096ede4
Josephine <jbl.noetic@gmail.com>
2025-03-01 21:07:10 -0600

    minigame scripting

--- my-react-app/src/components/Minigame.jsx
+++ my-react-app/src/components/Minigame.jsx
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Stats from './Stats'
 import Textbox from './Textbox' 
 import Book from './Book'
 import BookGUI from './BookGUI'
+import generateRequest from '../assets/updateStats.js'
+import requests from '../assets/requests.json'
 
-export default function Welcome() {
+export default function Minigame() {
+    let reqID;
+    useEffect(() => {
+        reqID = generateRequest(); console.log(reqID);
+    }, []);
+
     return (
         <div style={{backgroundImage: '../src/assets/Images/hall.webp'}}>
             <Book />
             <Stats />
-            <Textbox request={"PLEASE GIVE GOLD"} />
+            <Textbox request={requests.find(item => item.id === reqID)} />
         </div> 
     )
 }
--- my-react-app/src/components/Welcome.jsx
+++ my-react-app/src/components/Welcome.jsx
@@ -54,12 +54,12 @@ export default function Welcome() {
                     </p>
                     <p>
                         3) You start off with certain metrics, such as <u>happiness</u> and <u>wealth</u>. Happiness 
-                        represents how content your people are with your decisions, and wealth represents _____. You
+                        represents how content your people are with your decisions, and wealth represents how much weath you currently possess. You
                         want to keep happiness as high as possible to avoid any negative consequences.
                     </p>
                     <p>
                         4) You must balance happiness and wealth. If any of these go too low, you may face problems
-                        like _____.
+                        like being betrayed by your people or not having enough gold to keep the kingdom running.
                     </p>
                 </motion.div>
 
